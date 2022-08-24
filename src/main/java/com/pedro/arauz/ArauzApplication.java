10fc614a3b95c66e100b640f316ee443dc1e3418
Pedro Arauz <pedro.arauz.88@gmail.com>
2022-08-24 09:56:24 -0500

    first commit

---
+++ src/main/java/com/pedro/arauz/ArauzApplication.java
@@ -0,0 +1,13 @@
+package com.pedro.arauz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArauzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArauzApplication.class, args);
+	}
+
+}
